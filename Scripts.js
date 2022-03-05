828eacb4d160632753a6dff028db6709ad26abdf
Luiz Santos <luizaug10@gmail.com>
2022-03-05 10:41:26 -0300

    commit

---
+++ Scripts.js
@@ -0,0 +1,7 @@
+const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.querySelector("[data-board]");
+const winningMessageTextElement = document.querySelector(
+  "[data-winning-message-text]"
+);
+const winningMessage = document.querySelector("[data-winning-message]");
+const restartButton = document.querySelector("[data-restart-button]");
